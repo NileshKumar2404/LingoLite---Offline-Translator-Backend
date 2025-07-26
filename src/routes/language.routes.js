32ba5185f8972e0072b01aa48548d0d5a499e3c4
Nilesh <nileshkr2404@gmail.com>
2025-07-26 20:09:32 +0530

    Refactor upload-languagepack route to include multer file upload handling

--- src/routes/language.routes.js
+++ src/routes/language.routes.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 import { getAllLanguagePacks, uploadLanguagePack } from "../controllers/language.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router()
 
-router.route("/upload-languagepack").post(verifyJWT, uploadLanguagePack)
+router.route("/upload-languagepack").post(
+    upload.fields([
+        {
+            name: 'fileUrl', 
+            maxCount: 1
+        }
+    ]),
+    verifyJWT,
+    uploadLanguagePack
+)
 router.route("/get-language-pack").get(verifyJWT, getAllLanguagePacks)
 
 export default router
